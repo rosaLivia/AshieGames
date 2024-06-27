3a1a2df35d399766710e4ae77129472b87b8f24b
rosaLivia <127208803+rosaLivia@users.noreply.github.com>
2024-06-27 18:32:15 -0400

    Campo de endereço e cidade atualizados a partir de consultas

--- Trabalho3ProgMob/app/src/main/java/com/example/stardewvalley/service/UserService.java
+++ Trabalho3ProgMob/app/src/main/java/com/example/stardewvalley/service/UserService.java
@@ -2,11 +2,13 @@ package com.example.stardewvalley.service;
 
 import android.util.Log;
 
+import com.example.stardewvalley.entity.Seed;
 import com.example.stardewvalley.entity.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -14,10 +16,13 @@ public class UserService {
 
     private final FirebaseFirestore firestore;
     private final CollectionReference userCollection;
-
+    private final CollectionReference seedCollection;
+    private final CollectionReference cityCollection;
     public UserService() {
         firestore = FirebaseFirestore.getInstance();
         userCollection = firestore.collection("users");
+        seedCollection = firestore.collection("seeds");
+        cityCollection = firestore.collection("cities");
     }
 
     public void addUser(User user, OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener) {
@@ -35,5 +40,17 @@ public class UserService {
 
     public void getUserByEmail(String email, OnCompleteListener<QuerySnapshot> onCompleteListener) {
         userCollection.whereEqualTo("email", email).get().addOnCompleteListener(onCompleteListener);
+    }
+
+    public void getSeedById(String seedId, OnCompleteListener<DocumentSnapshot> onCompleteListener) {
+        seedCollection.document(seedId).get().addOnCompleteListener(onCompleteListener);
+    }
+
+    public void getCityById(String cityId, OnCompleteListener<DocumentSnapshot> onCompleteListener) {
+        cityCollection.document(cityId).get().addOnCompleteListener(onCompleteListener);
+    }
+
+    public void getUserDetails(String userId, OnCompleteListener<DocumentSnapshot> onCompleteListener) {
+        userCollection.document(userId).get().addOnCompleteListener(onCompleteListener);
     }
 }
